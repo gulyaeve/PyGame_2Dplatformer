15b6d7563d3c541fab124c83d1291a53ad35a614
gulyaeve <baddman@list.ru>
2023-08-17 10:42:25 +0300

    Added base events and requi

--- app.py
+++ app.py
@@ -1,2 +1,29 @@
+import sys
+
 import pygame
 
+WIDTH = 400
+HEIGHT = 300
+BACKGROUND = (0, 0, 0)
+
+
+def game():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    while True:
+        screen.fill(BACKGROUND)
+        pygame.display.flip()
+
+        clock.tick(60)  # FPS
+        print(clock.get_fps())
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+
+if __name__ == '__main__':
+    game()
