21454d78daec85805fa94c68cb49742a234758d3
gulyaeve <baddman@list.ru>
2023-08-17 12:48:17 +0300

    Added animation

--- player.py
+++ player.py
@@ -7,10 +7,24 @@ class Player(Sprite):
     def __init__(self, startx, starty):
         super().__init__("sprites/player/P1.png", startx, starty)
 
+        self.stand_image = self.image
+        self.walk_cycle = [pygame.image.load(f"sprites/player/P_walk_{i:0>2}.png") for i in range(1, 2)]
+        self.animation_index = 0
+        self.facing_left = False
+
         self.speed = 4
         self.jump = 20
         self.gravity = 1
         self.vertical_speed = 0
+
+    def walk_animation(self):
+        self.image = self.walk_cycle[self.animation_index]
+        if self.facing_left:
+            self.image = pygame.transform.flip(self.image, True, False)
+        if self.animation_index < len(self.walk_cycle)-1:
+            self.animation_index += 1
+        else:
+            self.animation_index = 0
 
     def update(self, boxes) -> None:
         horizontal_speed = 0
@@ -18,9 +32,15 @@ class Player(Sprite):
 
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT]:
+            self.facing_left = True
+            self.walk_animation()
             horizontal_speed = -self.speed
-        if key[pygame.K_RIGHT]:
+        elif key[pygame.K_RIGHT]:
+            self.facing_left = False
+            self.walk_animation()
             horizontal_speed = self.speed
+        else:
+            self.image = self.stand_image
         if key[pygame.K_UP] and onground:
             self.vertical_speed = -self.jump
         if self.vertical_speed < 10 and not onground:
